af819a9e318e428943981fe4082096a4aa6c34f2
escritorioagro2f <escritorio.agro2f@gmail.com>
2026-05-25 08:30:04 -0300

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,86 +1,31 @@
-import requests
-import hashlib
-import hmac
-import time
-import json
+import requests, hashlib, hmac, time, json
 
-TUYA_CLIENT_ID = "scd8n7dxm74yarxvfjgg"
-TUYA_CLIENT_SECRET = "5486c3b4c40844ddabb1adad1b9a2d06"
-TUYA_DEVICE_ID = "eb0238bf2ba8be7e86npyl"
-TUYA_BASE_URL = "https://openapi.tuyaus.com"
+CID = "scd8n7dxm74yarxvfjgg"
+SEC = "5486c3b4c40844ddabb1adad1b9a2d06"
+DEV = "eb0238bf2ba8be7e86npyl"
+URL = "https://openapi.tuyaus.com"
+WID = "ITAIAU3"
+WKY = "vqz5PM0x"
 
-WU_STATION_ID = "ITAIAU3"
-WU_STATION_KEY = "vqz5PM0x"
+def sg(msg):
+    return hmac.new(SEC.encode(), msg.encode(), hashlib.sha256).hexdigest().upper()
 
+def token():
+    t = str(int(time.time()*1000))
+    h = {"client_id":CID,"sign":sg(CID+t),"t":t,"sign_method":"HMAC-SHA256"}
+    r = requests.get(URL+"/v1.0/token?grant_type=1", headers=h)
+    print("TOKEN:", r.text)
+    return r.json()["result"]["access_token"]
 
-def sign(secret, msg):
-    return hmac.new(
-        secret.encode("utf-8"),
-        msg.encode("utf-8"),
-        hashlib.sha256
-    ).hexdigest().upper()
+def status(tk):
+    t = str(int(time.time()*1000))
+    p = "/v1.0/devices/"+DEV+"/status"
+    h = {"client_id":CID,"access_token":tk,"sign":sg(CID+tk+t+p),"t":t,"sign_method":"HMAC-SHA256"}
+    r = requests.get(URL+p, headers=h)
+    print("STATUS:", r.text)
+    d = {i["code"]:i["value"] for i in r.json()["result"]}
+    return d
 
-
-def get_token():
-    t = str(int(time.time() * 1000))
-    string_to_sign = TUYA_CLIENT_ID + t
-    headers = {
-        "client_id": TUYA_CLIENT_ID,
-        "sign": sign(TUYA_CLIENT_SECRET, string_to_sign),
-        "t": t,
-        "sign_method": "HMAC-SHA256",
-        "Content-Type": "application/json"
-    }
-    url = TUYA_BASE_URL + "/v1.0/token?grant_type=1"
-    r = requests.get(url, headers=headers)
-    print("TOKEN RESPOSTA:", r.text)
-    resp = r.json()
-    if not resp.get("success"):
-        raise Exception("Erro token: " + str(resp))
-    return resp["result"]["access_token"]
-
-
-def get_status(token):
-    t = str(int(time.time() * 1000))
-    path = "/v1.0/devices/" + TUYA_DEVICE_ID + "/status"
-    string_to_sign = TUYA_CLIENT_ID + token + t + path
-    headers = {
-        "client_id": TUYA_CLIENT_ID,
-        "access_token": token,
-        "sign": sign(TUYA_CLIENT_SECRET, string_to_sign),
-        "t": t,
-        "sign_method": "HMAC-SHA256",
-        "Content-Type": "application/json"
-    }
-    r = requests.get(TUYA_BASE_URL + path, headers=headers)
-    print("DEVICE RESPOSTA:", r.text)
-    resp = r.json()
-    if not resp.get("success"):
-        raise Exception("Erro device: " + str(resp))
-    data = {}
-    for item in resp["result"]:
-        data[item["code"]] = item["value"]
-    print("Dados:", json.dumps(data, indent=2))
-    return data
-
-
-def upload(data):
-    temp_raw = data.get("temp_current", data.get("va_temperature", 0))
-    hum = data.get("humidity_value", data.get("va_humidity", 0))
-    wind_raw = data.get("wind_speed", 0)
-    rain_raw = data.get("rain_hourly", data.get("rainfall", 0))
-    press_raw = data.get("pressure_value", data.get("air_pressure", 0))
-
-    temp_c = temp_raw / 10.0
-    temp_f = round(temp_c * 9.0 / 5.0 + 32, 1)
-    wind_mph = round((wind_raw / 10.0) * 2.23694, 1)
-    rain_in = round((rain_raw / 10.0) * 0.0393701, 3)
-    press_inhg = round((press_raw / 10.0) * 0.02953, 2)
-
-    params = {
-        "ID": WU_STATION_ID,
-        "PASSWORD": WU_STATION_KEY,
-        "tempf": temp_f,
-        "humidity": hum,
-        "windspeedmph": wind_mph,
-    
+tk = token()
+d = status(tk)
+print("DADOS:", json.dumps(d, indent=2))
