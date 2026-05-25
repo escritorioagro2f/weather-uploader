0e3279c3f38bfa5233eca3e99a1740c9f840af98
escritorioagro2f <escritorio.agro2f@gmail.com>
2026-05-25 08:41:45 -0300

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,6 +1,4 @@
-import tinytuya
-import requests
-import json
+import tinytuya, requests, json
 
 DEV = "eb0238bf2ba8be7e86npyl"
 WID = "ITAIAU3"
@@ -13,5 +11,27 @@ c = tinytuya.Cloud(
     apiDeviceID=DEV
 )
 
-result = c.getstatus(DEV)
-print("DADOS:", json.dumps(result, indent=2))
+r = c.getstatus(DEV)
+d = {i["code"]: i["value"] for i in r["result"]}
+
+temp_f  = round(d["temp_current_external"] / 10 * 9/5 + 32, 1)
+hum     = d["humidity_outdoor"]
+wind    = round(d["windspeed_avg"] * 0.621371, 1)
+gust    = round(d["windspeed_gust"] * 0.621371, 1)
+rain_h  = round(d["rain_1h"] * 0.0393701, 3)
+rain_d  = round(d["rain_24h"] * 0.0393701, 3)
+press   = round(d["atmospheric_pressture"] * 0.02953, 2)
+uv      = d["uv_index"]
+dew_f   = round(d["dew_point_temp"] / 10 * 9/5 + 32, 1)
+
+url = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
+params = {
+    "ID": WID, "PASSWORD": WKY, "action": "updateraw", "dateutc": "now",
+    "tempf": temp_f, "humidity": hum, "windspeedmph": wind,
+    "windgustmph": gust, "rainin": rain_h, "dailyrainin": rain_d,
+    "baromin": press, "UV": uv, "dewptf": dew_f
+}
+
+resp = requests.get(url, params=params)
+print("WU:", resp.text)
+print(f"Temp: {temp_f}F | Hum: {hum}% | Press: {press}inHg | UV: {uv}")
